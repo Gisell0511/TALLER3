27c6e023f0f0a225ea2e9af1b88cc9fbfbfcf54c
Gisell0511 <giseorozco05@gmail.com>
2024-10-30 23:06:39 -0500

    registe

---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterForm from '../components/RegisterForm';
+
+function Register() {
+  return (
+    <div>
+      <h2>Registro de Usuario</h2>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default Register;
